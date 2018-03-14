ce24a2a733def7267f5dc3438dd3ad308ef0ec91
Berengario <dm3-1-13.17@uni.eus>
2018-03-14 13:10:07 +0100

    Lortuta TXO!

---
+++ Poyecxto/pizzeria.py
@@ -0,0 +1,23 @@
+'''
+Created on 7 mar. 2018
+
+@author: Eider
+'''
+
+
+
+
+class Pizzeria:
+    def __init__(self, izena, helbidea, telefonoa):
+        self.izena=izena
+        self.helbidea=helbidea
+        self.telefonoa=telefonoa
+        
+class Edaria:
+    def __init__(self):
+        print("kaixo")
+class Janaria:
+    def __init__(self):  
+        
+                
+     
